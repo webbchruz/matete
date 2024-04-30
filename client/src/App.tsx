d81c07428f163734ba659c1a8de109c5648da167
webbchruz <35582236+webbchruz@users.noreply.github.com>
2024-04-30 02:28:33 +0100

    implimentation of ts

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <div>
+      <header>Ts matete</header>
+      Ts matete
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
